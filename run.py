69fbd9b599e1410b78f8980b1e5659f553a4ee59
jareddishman <jareddishman@gmail.com>
2024-03-08 16:28:26 -0800

    Added csv output for results

--- run.py
+++ run.py
@@ -4,12 +4,15 @@ import sys
 import src.features.build_datasets as build
 import src.models.train_model as train_model
 import src.models.predict_model as predict
+import src.output.output as output
 
 import time
 import numpy as np
 
-test_path = "/projects/greengenes2/gg2_genomes/ncbi/GCA/018/937/935/GCA_018937935.1_PDT001069856.1/GCA_018937935.1_PDT001069856.1_protein.faa.gz"
+# test_path = "/projects/greengenes2/gg2_genomes/ncbi/GCA/018/937/935/GCA_018937935.1_PDT001069856.1/GCA_018937935.1_PDT001069856.1_protein.faa.gz"
 # test_path = "/projects/greengenes2/gg2_genomes/ncbi/GCF/002/967/575/GCF_002967575.1_BS951/GCF_002967575.1_BS951_protein.faa.gz"
+
+paths = ["/projects/greengenes2/gg2_genomes/ncbi/GCA/018/937/935/GCA_018937935.1_PDT001069856.1/GCA_018937935.1_PDT001069856.1_protein.faa.gz", "/projects/greengenes2/gg2_genomes/ncbi/GCF/002/967/575/GCF_002967575.1_BS951/GCF_002967575.1_BS951_protein.faa.gz"]
 
 def main():
     # parser = argparse.ArgumentParser(description='Predict whether a peptide has antimicrobial properties using a Deep Neural Network')
@@ -19,14 +22,18 @@ def main():
 
     # files = sys.stdin
     # if len(files) == 0: files = test_path # temporary to just allow running the damn thing
-    files = test_path
-
-
     decision_threshold=0.99
-    prediction = predict.predict_from_file(model, files, decision_threshold=decision_threshold)
-    # headers = prediction['headers']
+    for path in paths:
+        print('processing file at {path}')
+        prediction = predict.predict_from_file(model, path, decision_threshold=decision_threshold)
 
-    show_metrics(prediction, decision_threshold=decision_threshold)
+        show_metrics(prediction, decision_threshold=decision_threshold)
+
+        base_path = 'outputs/'
+        # output_path = base_path + path.split('/')[-1].split('.')[0] + '.csv' # using only GCA_ value
+        output_path = base_path + path.split('/')[-1][:-7] + '.csv' # using GCA_ with 1_PDT value
+
+        output.dict_to_csv(output_path, prediction, filtered_keys='labels')
 
 def show_metrics(prediction, **kwargs):
     headers = prediction['headers']
@@ -39,11 +46,7 @@ def show_metrics(prediction, **kwargs):
     print(f'num positive for AMP = {num_positive}. Total sequences processed = {len(prediction["labels"])}')
     print(f'AMP detection rate with threshold of {kwargs["decision_threshold"]} = {num_positive/len(prediction["labels"])}')
     # print({k:v for k,v in zip(prediction['headers'], prediction['raw'])})
-    print({k:v for k,v in zip(prediction['headers'], prediction['confidence'])})
-    
-
-def write_output(prediction):
-    ...
+    # print({k:v for k,v in zip(prediction['headers'], prediction['confidence'])})
 
 
 if __name__ == '__main__':
